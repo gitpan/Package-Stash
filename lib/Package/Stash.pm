d814e68a73b9784464d3489af2da9ba3119d9dfa
Jesse Luehrs <doy@cpan.org>
2011-01-11 17:45:07 +0000

    Import of DOY/Package-Stash-0.23 from CPAN.
    gitpan-cpan-distribution: Package-Stash
    gitpan-cpan-version:      0.23
    gitpan-cpan-path:         DOY/Package-Stash-0.23.tar.gz
    gitpan-cpan-author:       DOY
    gitpan-cpan-maturity:     released

--- lib/Package/Stash.pm
+++ lib/Package/Stash.pm
@@ -1,6 +1,6 @@
 package Package::Stash;
 BEGIN {
-  $Package::Stash::VERSION = '0.22';
+  $Package::Stash::VERSION = '0.23';
 }
 use strict;
 use warnings;
@@ -102,7 +102,7 @@ Package::Stash - routines for manipulating stashes
 
 =head1 VERSION
 
-version 0.22
+version 0.23
 
 =head1 SYNOPSIS
 
